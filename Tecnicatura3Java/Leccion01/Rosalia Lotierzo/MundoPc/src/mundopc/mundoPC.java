647fc50643a74ef1690080373f87fc0f86e2f391
Rosalía <84238521+Ro07-r@users.noreply.github.com>
2023-04-20 22:24:47 -0300

    Leccion01 continuacion

---
+++ Tecnicatura3Java/Leccion01/Rosalia Lotierzo/MundoPc/src/mundopc/mundoPC.java
@@ -0,0 +1,18 @@
+package mundopc;
+
+import ar.com.system2023.mundopc.*; //Con * se importan todas las clases que estén dentro del paquete
+
+public class mundoPC {
+    public static void main(String[] args) {
+        Monitor monitorHP = new Monitor("HP", 13); //Importar la clase
+        Teclado tecladoHP = new Teclado("Bluetooth", "HP");
+        Raton ratonHP = new Raton("Bluetooth", "HP");
+        Computadora computadoraHP = new Computadora("Computadora HP", monitorHP, tecladoHP, ratonHP);
+        
+        //Creacion de otros objetos de diferente marca
+        Monitor monitorGamer = new Monitor("Gamer", 32); 
+        Teclado tecladoGamer = new Teclado("Bluetooth", "Gamer");
+        Raton ratonGamer = new Raton("Bluetooth", "Gamer");
+        Computadora computadoraGamer = new Computadora("Computadora Gamer", monitorGamer, tecladoGamer, ratonGamer);
+    }
+}
