17850627456f7b764f033151c66f723ad63ca622
Emmanueltoro28 <111504496+Emmanueltoro28@users.noreply.github.com>
2023-05-01 18:43:04 -0300

    Leccion02

---
+++ Tecnicatura3Python/Leccion02/ManejoArchivos.py
@@ -0,0 +1,12 @@
+class ManejoArchivos:
+    def __init__(self, nombre):
+        self.nombre = nombre
+    def __enter__(self):
+        print('Obtenemos el recurso'.center(50, '-'))
+        self.nombre = open(self.nombre, 'r', encoding='utf8') #Encapsulamos el código dentro del método
+        return self.nombre
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('Cerramos el recurso'.center(50, '-'))
+        if self.nombre:
+            self.nombre.close() #Cerramos el archivo
