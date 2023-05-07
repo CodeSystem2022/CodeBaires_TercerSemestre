4e2d19238a89f7ba4c2f14dbdd0f66cdfdf8a64b
Maria Rodriguez <marialrodriguez027@gmail.com>
2023-05-07 20:26:35 -0300

    subo ejercicios instanceOf

--- Tecnicatura3Java/Leccion04/Polimorfismo/src/domain/Gerente.java
+++ Tecnicatura3Java/Leccion04/Polimorfismo/src/domain/Gerente.java
@@ -13,5 +13,13 @@ public class Gerente extends Empleado {
     public String obtenerDetalles() {
         return super.obtenerDetalles()+", Departamento: "+this.departamento;
     }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
     
 }
