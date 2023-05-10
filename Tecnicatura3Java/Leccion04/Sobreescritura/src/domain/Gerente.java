ccc82633341bc8d30c1acfb548ae55a1357f1bb4
DeisyOrtega <et5ortegadeisy@gmail.com>
2023-05-10 01:00:40 -0300

    subo leccion04parte1y2

---
+++ Tecnicatura3Java/Leccion04/Sobreescritura/src/domain/Gerente.java
@@ -0,0 +1,14 @@
+
+package domain;
+
+
+public class Gerente extends Empleado {
+    private String departamento;
+    
+    public Gerente(String nombre, double sueldo, String departamento){
+        super(nombre, sueldo);
+        this.departamento = departamento;
+    }
+    
+    
+}
