40745233daea9ba40c96a228f344e54734eb0918
MaiaStrusiat <112667066+MaiaStrusiat@users.noreply.github.com>
2023-05-16 17:39:35 +0200

    Subo JAVA, Pruebas con Downcasting y Upcasting: Parte 2

--- Tecnicatura3Java/Leccion05/ConversionObjetos/src/test/TestConversionObjetos.java
+++ Tecnicatura3Java/Leccion05/ConversionObjetos/src/test/TestConversionObjetos.java
@@ -5,11 +5,11 @@ public class TestConversionObjetos {
     public static void main(String[] args) {
         Empleado empleado;
         empleado = new Escritor("juan", 5000, TipoEscritura.CLASICO);
-        System.out.println("empleado = "+empleado);
-        System.out.println(empleado.obtenerDetalles()); // si queremos acceder a metodos Escritor
+        //System.out.println("empleado = "+empleado);
+        //System.out.println(empleado.obtenerDetalles()); // si queremos acceder a metodos Escritor
         //empleado.getTipoEscritura(); //no se puede hacer 
         //Dowcasting
-       // ((Escritor)empleado).getTipoEscritura();//tenemos 2 opciones : esta es una 
+        // ((Escritor)empleado).getTipoEscritura();//tenemos 2 opciones : esta es una 
         Escritor escritor = (Escritor)empleado; // esta es la segunda opcion
         escritor.getTipoEscritura();
         
