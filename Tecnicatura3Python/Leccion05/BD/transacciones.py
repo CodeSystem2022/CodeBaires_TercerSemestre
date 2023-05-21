535cb5c2acf98fda40b44ebe5a5a7a1fa9e22756
Emmanueltoro28 <111504496+Emmanueltoro28@users.noreply.github.com>
2023-05-20 22:28:46 -0300

    subo transacciones parte 1

---
+++ Tecnicatura3Python/Leccion05/BD/transacciones.py
@@ -0,0 +1,14 @@
+import psycopg2 as bd # Esto es para poder conectarnos a Postgre
+
+conexion = bd.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
+try:
+    conexion.autocommit = False
+    cursor = conexion.cursor()
+    sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
+    valores = ('Maria', 'Esparza', 'mesparza@mail.com')
+    cursor.execute(sentencia, valores)
+    print('Termina la transacción')
+except Exception as e:
+    print(f"Ocurrió un error: {e}")
+finally:
+    conexion.close()
