b835adcb1dbd65c0c86f2341e6755408681b554f
= <yamila.carlucci@gmail.com>
2023-05-20 21:15:17 -0300

    JS-tarea asignada en el video

--- Tecnicatura3JS/Leccion06/05-03-clases.js
+++ Tecnicatura3JS/Leccion06/05-03-clases.js
@@ -14,16 +14,30 @@ class Persona{
     set nombre(nombre){
         this._nombre=nombre;
     }
+
+    get apellido(){
+        return this._apellido;
+    }
+
+    set apellido(apellido){
+        this._apellido=apellido;
+    }
 }
+    
+
 
 let persona1 = new Persona("Martin","Perez");    // creo OBJETO persona1 y persona2 con el metodo constructor de la clase Persona que me pide los 2 atributos como parametros
+console.log(persona1);
 console.log(persona1.nombre);
 persona1.nombre='Juan Carlos';
 console.log(persona1.nombre);
-//console.log(persona1);
+persona1.apellido="Rodriguez";
+console.log(persona1)
+
 
 let persona2 = new Persona('Carlos','Lara');     
 console.log(persona2.nombre);
 persona2.nombre='Maria Laura';
 console.log(persona2.nombre);
-//console.log(persona2);
+persona2.apellido = "Juarez";
+console.log(persona2);
