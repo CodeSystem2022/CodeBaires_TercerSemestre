ca4f25aa63cf8ad42cbd1f5ab0382e4de219c588
Maria Rodriguez <marialrodriguez027@gmail.com>
2023-05-22 19:08:45 -0300

    subo transacciones parte 02

--- Tecnicatura3Python/Leccion06/BD/transacciones.py
+++ Tecnicatura3Python/Leccion06/BD/transacciones.py
@@ -2,13 +2,15 @@ import psycopg2 as bd # Esto es para poder conectarnos a Postgre
 
 conexion = bd.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
 try:
-    conexion.autocommit = False
+   # conexion.autocommit = False # esto directamente no deberia estar
     cursor = conexion.cursor()
     sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
     valores = ('Maria', 'Esparza', 'mesparza@mail.com')
     cursor.execute(sentencia, valores)
+   conexion.commit() # Hacemos el commit manualmente
     print('Termina la transacción')
 except Exception as e:
-    print(f"Ocurrió un error: {e}")
+    conexion.rollback()
+    print(f"Ocurrió un error, se hizo un rollback: {e}")
 finally:
     conexion.close()
