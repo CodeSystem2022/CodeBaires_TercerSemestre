29bb7599842e7245bff5519d0efbcbc211f2bd2f
= <yamila.carlucci@gmail.com>
2023-05-24 22:05:19 -0300

    Subo carpetas lecciones

---
+++ Tecnicatura3JS/Leccion07/05-03-clases.js
@@ -0,0 +1,43 @@
+//let persona3 = new Persona('Carla', 'Ponce');// NO SE PUEDE HACER: Persona is not defined. 
+
+class Persona{
+
+    constructor(nombre, apellido){      //Creo constructor y paso como parametros ambos atributos: nombre y apellido
+        this._nombre = nombre;          // escribo _nombre, _apellido para diferenciar el ATRIBUTO o PROPERTY(propiedad)
+        this._apellido = apellido;      // del metodo get y set que luego creare, en javascript no pueden llevar el mismo nombre (como en java)
+    }
+  
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre=nombre;
+    }
+   //  Cramos el metodo get y set para apellido 
+    get apellido(){ 
+        return this._apellido;
+    }
+
+    set apellido(apellido){
+        this._apellido=apellido;
+    }
+}
+    
+
+
+let persona1 = new Persona("Martin","Perez");    // creo OBJETO persona1 y persona2 con el metodo constructor de la clase Persona que me pide los 2 atributos como parametros
+console.log(persona1);
+console.log(persona1.nombre);
+persona1.nombre='Juan Carlos';
+console.log(persona1.nombre);
+persona1.apellido="Rodriguez";
+console.log(persona1)
+
+
+let persona2 = new Persona('Carlos','Lara');     
+console.log(persona2.nombre);
+persona2.nombre='Maria Laura';
+console.log(persona2.nombre);
+persona2.apellido = "Juarez";
+console.log(persona2);
