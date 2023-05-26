6deb2d3991d772c84a9546f7a16242c90e82ac58
Matias Quispe <111471872+matiasq3@users.noreply.github.com>
2023-05-26 17:31:03 -0300

    JS - Leccion7 - 7.1 Heredar métodos
    Agrego: JS - Leccion7 - 7.1 Heredar métodos#143

--- Tecnicatura3JS/Leccion07/05-03-clases.js
+++ Tecnicatura3JS/Leccion07/05-03-clases.js
@@ -1,6 +1,6 @@
 //let persona3 = new Persona('Carla', 'Ponce');// NO SE PUEDE HACER: Persona is not defined. 
 
-class Persona{
+class Persona{ //Clase padre
 
     constructor(nombre, apellido){      //Creo constructor y paso como parametros ambos atributos: nombre y apellido
         this._nombre = nombre;          // escribo _nombre, _apellido para diferenciar el ATRIBUTO o PROPERTY(propiedad)
@@ -22,6 +22,23 @@ class Persona{
     set apellido(apellido){
         this._apellido=apellido;
     }
+    // Metodo nombreCompleto 
+    nombreCompleto(){
+        return this._nombre+' '+this._apellido;  
+    }
+}
+
+class Empleado extends Persona{// Clase hija 
+    constructor(nombre, apellido, departamento){
+        super(nombre, apellido);
+        this._departamento = departamento;
+    }
+    get departamento(){
+        return this._departamento;
+    }
+    set departamento(departamento){
+        this._departamento = departamento;
+    }
 }
     
 
@@ -34,10 +51,17 @@ console.log(persona1.nombre);
 persona1.apellido="Rodriguez";
 console.log(persona1)
 
-
 let persona2 = new Persona('Carlos','Lara');     
 console.log(persona2.nombre);
 persona2.nombre='Maria Laura';
 console.log(persona2.nombre);
 persona2.apellido = "Juarez";
 console.log(persona2);
+
+let empleado1 = new Empleado('Maria', 'Gimenez', 'Sistemas');
+console.log(empleado1);
+console.log(empleado1.nombre);
+
+console.log(empleado1.nombreCompleto()); //la clase hija heredo el metodo nombreCompleto 
+
+
