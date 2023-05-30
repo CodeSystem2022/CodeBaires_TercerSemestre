0ef6ffb7e051c3ee9d0d8db99712f9552f637e9b
= <yamila.carlucci@gmail.com>
2023-05-30 08:07:33 -0300

    Update TestInterfaces.java

--- Tecnicatura3Java/Leccion06/Interfaces/src/test/TestInterfaces.java
+++ Tecnicatura3Java/Leccion06/Interfaces/src/test/TestInterfaces.java
@@ -7,8 +7,14 @@ public class TestInterfaces {
         
         IAccesoDatos datos = new ImplementacionMySql();
         //datos.listar();
+        //imprimir(datos);
         
         datos = new ImplementacionOracle();
+        //datos.listar();
+        imprimir(datos);
+    }
+    
+    public static void imprimir(IAccesoDatos datos){
         datos.listar();
     }
 }
