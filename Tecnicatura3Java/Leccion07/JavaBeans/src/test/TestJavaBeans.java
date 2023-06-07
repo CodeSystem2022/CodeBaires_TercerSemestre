83169cee55abb3da4f7766bb4451a5de966af660
Marina Falcon <falcon.marina.it@gmail.com>
2023-06-07 02:27:37 -0300

    subo leccion 08.2 JAVA
    JAVA Leccion08.2 Pruebas de JavaBeans

---
+++ Tecnicatura3Java/Leccion07/JavaBeans/src/test/TestJavaBeans.java
@@ -0,0 +1,18 @@
+
+package test;
+
+import domain.Persona;
+
+
+public class TestJavaBeans {
+    public static void main(String[] args) {
+         Persona persona = new Persona();
+         persona.setNombre("Juan");
+         persona.setApellido("Perez");
+         System.out.println("Persona =" + persona);
+         
+         System.out.println("Persona nombre: " + persona.getNombre());
+         System.out.println("Persona apellido " + persona.getApellido());
+    }
+   
+}
