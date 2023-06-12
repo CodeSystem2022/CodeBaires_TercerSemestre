ccf86287894d661119fc874d8fe81b9ef6f6dd1d
= <yamila.carlucci@gmail.com>
2023-06-12 08:20:14 -0300

    JAVA-Leccion09-9.3-Ponemos la entrada de informacion

--- Tecnicatura3Java/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
+++ Tecnicatura3Java/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
+
 public class CalculadoraUTN {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
         System.out.println("********** Aplicacion Calculadora **********");
-        var operando1 = 10;
-        var operando2 = 20;
+        System.out.print("Ingrese el valor para el operando1: ");
+        var operando1 = Integer.parseInt(entrada.nextLine());
+        System.out.print("Ingrese el valor para el operando2: ");
+        var operando2 = Integer.parseInt(entrada.nextLine());
         var resultado = operando1 + operando2;
         System.out.println("resultado = " + resultado);
-        
-        
-
     }
 }
