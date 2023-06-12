195ea5551fba914b373223ddc36710e508ceef00
Marina Falcon <falcon.marina.it@gmail.com>
2023-06-12 04:07:44 -0300

    Subo Leccion09-9.2 Definicion de variables
    Subo leccion09-9,2 Definicion de variables

---
+++ Tecnicatura3Java/Leccion09/CalculadoraUTN/CalculadoraUTN/src/CalculadoraUTN.java
@@ -0,0 +1,13 @@
+public class CalculadoraUTN {
+    public static void main(String[] args) {
+        System.out.println("********** Aplicacion Calculadora **********");
+        var operando1 = 10;
+        var operando2 = 20;
+        var resultado = operando1 + operando2;
+        System.out.println("resultado = " + resultado);
+        
+
+
+
+    }
+}
