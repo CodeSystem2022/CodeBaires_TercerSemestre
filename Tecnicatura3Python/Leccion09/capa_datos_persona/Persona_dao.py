bb4c7e7597648cdc75a16f3b49b1b20efb427974
= <yamila.carlucci@gmail.com>
2023-06-14 20:58:48 -0300

    Update Persona_dao.py

--- Tecnicatura3Python/Leccion09/capa_datos_persona/Persona_dao.py
+++ Tecnicatura3Python/Leccion09/capa_datos_persona/Persona_dao.py
@@ -48,18 +48,32 @@ class PersonaDAO:
                 log.debug(f'Persona Actualizada: {persona}')
                 return cursor.rowcount
 
+    @classmethod
+    def eliminar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.id_persona)
+                cursor.execute(cls._ELIMINAR, valores)
+                log.debug(f'Persona Eliminada: {persona}')
+                return cursor.rowcount
+
 if __name__ == '__main__':
 
     # Actualizar un registro
-    persona1 = Persona(1, 'Juan José', 'Pena', 'jjpena@mail.com')
-    personas_actualizadas = PersonaDAO.actualizar(persona1)
-    log.debug(f'Personas actualizadas: {personas_actualizadas}')
+    # persona1 = Persona(1, 'Juan José', 'Pena', 'jjpena@mail.com')
+    # personas_actualizadas = PersonaDAO.actualizar(persona1)
+    # log.debug(f'Personas actualizadas: {personas_actualizadas}')
 
 
     # Insertar un registro
     # persona1 = Persona(nombre='Homero', apellido='Ramos', email='homeror@mail.com')
     # personas_insertadas = PersonaDAO.insertar(persona1)
     # log.debug(f'Personas insertadas: {personas_insertadas}')
+
+    # Eliminar un registro
+    persona1 = Persona(id_persona=13)
+    personas_eliminadas = PersonaDAO.eliminar(persona1)
+    log.debug(f'Personas eliminadas: {personas_eliminadas}')
 
     # Seleccionar objetos
     personas = PersonaDAO.seleccionar()
