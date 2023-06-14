d3568f2628e9f05c925e489ac760adf082cb1683
DeisyOrtega <et5ortegadeisy@gmail.com>
2023-06-14 18:59:09 -0300

    Java-L9.5
    agregamos función switch de la lección 9.5 de JAVA.

--- Tecnicatura3Java/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
+++ Tecnicatura3Java/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -20,7 +20,38 @@ public class CalculadoraUTN {
             System.out.print("Ingrese el valor para el operando2: ");
             var operando2 = Integer.parseInt(entrada.nextLine());
 
+
+            int resultado;
+            switch (operacion){
+                case 1 -> {//suma
+                    resultado = operando1 + operando2;
+                    System.out.println(" Resultado de la suma: "+resultado);
+                }
+                case  2 -> {//resta
+                    resultado = operando1-operando2;
+                    System.out.println("Resultado de la resta: "+resultado);
+                }
+                case 3 -> {//miultiplicacion
+                    resultado = operando1*operando2;
+                    System.out.println("Resultado de la multiplicación: "+resultado);
+                }
+                case 4 -> {//division
+                    resultado = operando1/operando2;
+                    System.out.println("Resultado de la divisón: "+resultado);
+                }
+                default -> System.out.println("Opcion erronea: "+operacion);
+
+            }//fin switch
+
+        }// fin de if
+        else if (operacion == 5) {
+            System.out.println("Hasta pronto.....");
+        }
+        else {
+            System.out.println("Opcion erronea: "+operacion);
         }
 
-    }
-}
+        }// fin main
+
+    }// fin clase
+
