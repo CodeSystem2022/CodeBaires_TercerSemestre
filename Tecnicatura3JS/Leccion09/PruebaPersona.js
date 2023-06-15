f3f3a60c4eb5d20865c3a56cd1907736b8dba34e
Rosalía <84238521+Ro07-r@users.noreply.github.com>
2023-06-14 21:22:32 -0300

    JS-LECCION09-Cargo todo en una sola plantilla

--- Tecnicatura3JS/Leccion09/PruebaPersona.js
+++ Tecnicatura3JS/Leccion09/PruebaPersona.js
@@ -1,3 +1,110 @@
+class Persona{
+    static contadorPersonas = 0;
+
+    constructor(nombre, apellido, edad){
+        this._idPersona = ++Persona.contadorPersonas;
+        this._nombre = nombre;
+        this._apellido = apellido;
+        this._edad = edad;
+    }
+
+    get idPersona(){
+        return this._idPersona;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    get apellido(){
+        return this._apellido;
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+
+    get edad(){
+        return this._edad;
+    }
+
+    set edad(edad){
+        this._edad = edad;
+    }
+
+    toString(){
+        return `
+        ${this._idPersona}
+        ${this._nombre}
+        ${this._apellido}
+        ${this._edad}`;
+    }
+}
+
+class Empleado extends Persona{
+    static contadorEmpleados = 0;
+
+    constructor(nombre, apellido, edad, sueldo){
+        super(nombre, apellido, edad);
+        this._idEmpleado = ++Empleado.contadorEmpleados;
+        this._sueldo = sueldo;
+    }
+
+    get idEmpleado(){
+        return this._idEmpleado;
+    }
+
+    get sueldo(){
+        return this._sueldo;
+    }
+
+    set sueldo(sueldo){
+        this._sueldo = sueldo;
+    }
+
+    toString(){
+        return`
+        ${super.toString()}
+        ${this._idEmpleado}
+        ${this._sueldo}`;        
+    }
+}
+
+class Cliente extends Persona{
+
+    static contadorClientes = 0;
+
+    constructor(nombre, apellido, edad, fechaRegistro){
+        super(nombre, apellido, edad);
+        this._idCliente = ++Cliente.contadorClientes;
+        this._fechaRegistro = fechaRegistro;
+    }
+
+    get idCliente(){
+        return this._idCliente;
+    }
+
+    get fechaRegistro(){
+        return this._fechaRegistro;
+    }
+
+    set fechaRegistro(fechaRegistro){
+        this._fechaRegistro = fechaRegistro;
+    }
+
+    toString(){
+        return`
+        ${super.toString()}
+        ${this._idCliente}
+        ${this._fechaRegistro}`;
+    }
+}
+
+
 //Prueba clase Persona
 let persona1 = new Persona('Juan', 'Perez', 32);
 console.log(persona1.toString());
