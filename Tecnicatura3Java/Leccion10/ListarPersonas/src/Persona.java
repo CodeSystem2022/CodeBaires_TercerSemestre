1c06dca6b295e6b51a9f66ca28b26b561a49251b
Marina Falcon <falcon.marina.it@gmail.com>
2023-06-19 03:44:51 -0300

    Subo JAVA-Leccion10-10.2 Parte 3
    Creacion Clase Persona Parte 3

--- Tecnicatura3Java/Leccion10/ListarPersonas/src/Persona.java
+++ Tecnicatura3Java/Leccion10/ListarPersonas/src/Persona.java
@@ -18,4 +18,46 @@ public class Persona {
         this.email = email;
 
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", tel='" + tel + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
