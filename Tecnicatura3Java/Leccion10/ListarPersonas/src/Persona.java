33e46f49b513ce6d849daa925d99db63221e4e06
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-06-19 09:24:23 -0300

    Fix carpeta

--- Tecnicatura3Java/Leccion10/ListarPersonas/src/Persona.java
+++ Tecnicatura3Java/Leccion10/ListarPersonas/src/Persona.java
@@ -1,6 +1,5 @@
 public class Persona {
-    private  int id;
-    private String  nombre;
+    private String nombre;
     private String tel;
     private String email;
     private static int numeroPersonas = 0;
@@ -10,12 +9,4 @@ public class Persona {
         this.id = ++Persona.numeroPersonas;
     }
 
-    // constructor con parametros(sobrecarga de constructores)
-    public  Persona(String nombre, String tel, String email){
-        this();
-        this.nombre = nombre;
-        this.tel = tel;
-        this.email = email;
-
-    }
 }
