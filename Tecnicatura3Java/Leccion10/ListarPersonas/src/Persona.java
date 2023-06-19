cce8e48e892f117f30e31e1f59f771624ae49874
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-06-19 09:26:57 -0300

    Update Persona.java

--- Tecnicatura3Java/Leccion10/ListarPersonas/src/Persona.java
+++ Tecnicatura3Java/Leccion10/ListarPersonas/src/Persona.java
@@ -1,4 +1,5 @@
 public class Persona {
+    private int id;
     private String nombre;
     private String tel;
     private String email;
@@ -8,5 +9,18 @@ public class Persona {
     public Persona(){
         this.id = ++Persona.numeroPersonas;
     }
+
+    //Constructor con parámetros (sobrecarga de constructores)
+    public Persona(String nombre, String tel, String email){
+        this(); //Por default lo hace automáticamente
+        this.nombre = nombre;
+        this.tel = tel;
+        this.email = email;
+
+
+
+    }
+
+
 
 }
