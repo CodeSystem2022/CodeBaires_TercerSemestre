dbe889451a63877ea8e0679e9d88bd28753e0840
Matias Quispe <111471872+matiasq3@users.noreply.github.com>
2023-06-20 19:02:30 -0300

    JAVA-Leccion10-10.3-CreamosMenu-Parte1
    agrego: JAVA-Leccion10-10.3-CreamosMenu-Parte1

---
+++ Tecnicatura3Java/Leccion10/ListarPersonas/src/ListadoPersonasApp.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ListadoPersonasApp {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        // Definimos la lista fuera del ciclo while
+        List<Persona> persona = new ArrayList<>();
+
+        //Empezamos con el menu
+        var salir = false;
+        while (!salir){//salir = false
+            mostrarMenu();//funcion
+
+        }// Fin del ciclo While
+
+    }//Fin metodo menu
+}
