905880ab4e9258d578e48741ce7f8468a0de5504
Rosalía <84238521+Ro07-r@users.noreply.github.com>
2023-06-29 22:04:28 -0300

    Java-Leccion12-Listado de estudiantes(parte 3 y 4)

--- Tecnicatura3Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ Tecnicatura3Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -21,6 +21,29 @@ public class EstudianteDAO {
         //Creamos un objeto de tipo conexión
         Connection con = getConnection();
         String sql = "SELECT * FROM estudiantes ORDER BY estudiantes2022";
-
-    }
+        try{
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                var estudiante = new Estudiante();
+                estudiante.setIdEstudiante(rs.getInt("idestudiante2022"));
+                estudiante.setNombre(rs.getString("nombre"));
+                estudiante.setApellido(rs.getString("apellido"));
+                estudiante.setTelefono(rs.getString("telefono"));
+                estudiante.setEmail(rs.getString("email"));
+                //Falta agregarlo a la lista
+                estudiantes.add(estudiante);
+            }
+        } catch (Exception e){
+            System.out.println("Ocurrió un error al seleccionar datos: "+e.getMessage());
+        }
+        finally {
+            try{
+                CON.close();
+            } catch (Exception e){
+                System.out.println("Ocurrió un error al cerrar la conexión: "+e.getMessage());
+            }
+        }//Fin finally
+        return estudiantes;
+    }//Fin método Listar
 }
