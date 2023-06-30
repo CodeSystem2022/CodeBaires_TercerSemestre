ad924f3377542212214a8b2637d39bd1d5d8ba44
DeisyOrtega <et5ortegadeisy@gmail.com>
2023-06-30 18:21:02 -0300

    Java12.3
    subo parte 3 de la lección 12

--- Tecnicatura3Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ Tecnicatura3Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class EstudianteDAO {
 
     //Método Listar
-    public List<Estudiante> listar(){
+    public List<Estudiante> listarEstudiantes(){
         List<Estudiante> estudiantes = new ArrayList<>();
         //Creamos algunos objetos que son necesarios para comunicarnos
         //con la base de datos
@@ -20,13 +20,13 @@ public class EstudianteDAO {
         ResultSet rs; //Obtenemos el resultado de la base de datos
         //Creamos un objeto de tipo conexión
         Connection con = getConnection();
-        String sql = "SELECT * FROM estudiantes ORDER BY estudiantes2022";
+        String sql = "SELECT * FROM estudiantes2022 ORDER BY idestudiantes2022";
         try{
             ps = con.prepareStatement(sql);
             rs = ps.executeQuery();
             while(rs.next()){
                 var estudiante = new Estudiante();
-                estudiante.setIdEstudiante(rs.getInt("idestudiante2022"));
+                estudiante.setIdEstudiante(rs.getInt("idestudiantes2022"));
                 estudiante.setNombre(rs.getString("nombre"));
                 estudiante.setApellido(rs.getString("apellido"));
                 estudiante.setTelefono(rs.getString("telefono"));
@@ -46,4 +46,12 @@ public class EstudianteDAO {
         }//Fin finally
         return estudiantes;
     }//Fin método Listar
+
+    public static void main(String[] args) {
+        //listar los estudiantes
+        var estudianteDao = new EstudianteDAO();
+        System.out.println("Listado de estudiantes: ");
+        List<Estudiante> estudiantes = estudianteDao.listarEstudiantes();
+        //estudiantes.forEach(System.out::println);// funcion lambda para imprimir
+    }
 }
