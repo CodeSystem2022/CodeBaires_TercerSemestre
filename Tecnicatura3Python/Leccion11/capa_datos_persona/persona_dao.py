6b0474938800b46e0cb794f2ee530a7801c4091d
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-06-30 23:15:29 -0300

    Update persona_dao.py

--- Tecnicatura3Python/Leccion11/capa_datos_persona/persona_dao.py
+++ Tecnicatura3Python/Leccion11/capa_datos_persona/persona_dao.py
@@ -62,18 +62,18 @@ class PersonaDAO:
 if __name__ == '__main__':
 
     # Eliminar un registro
-    persona1 = Persona(id_persona=8)
+    persona1 = Persona(id_persona=18)
     personas_eliminadas = PersonaDAO.eliminar(persona1)
     log.debug(f'Personas eliminadas: {personas_eliminadas}')
 
     # Actualizar un registro
-    persona1 = Persona(1, 'Juan José', 'Pena', 'jjpena@mail.com')
+    persona1 = Persona(1, 'Juan', 'Pena', 'jpena@mail.com')
     personas_actualizadas = PersonaDAO.actualizar(persona1)
     log.debug(f'Personas actualizadas: {personas_actualizadas}')
 
 
     # Insertar un registro
-    persona1 = Persona(nombre='', apellido='Tejada', email='tejadam@mail.com')
+    persona1 = Persona(nombre='Mateo', apellido='Torres', email='tmateo@mail.com')
     personas_insertadas = PersonaDAO.insertar(persona1)
     log.debug(f'Personas insertadas: {personas_insertadas}')
 
